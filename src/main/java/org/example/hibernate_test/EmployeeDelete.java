5f1775746d9cdb675ce899211ffcef79e2217657
Egor <sartvas@yandex.ru>
2021-08-19 17:36:10 +0300

    Изменение одного или нескольких объектов в БД при помощи JAVA

--- src/main/java/org/example/hibernate_test/EmployeeDelete.java
+++ src/main/java/org/example/hibernate_test/EmployeeDelete.java
@@ -5,7 +5,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class EmployeeAdd {
+public class EmployeeDelete {
     public static void main(String[] args) {
         SessionFactory factory = new Configuration()
                 .configure("hibernate.cfg.xml")
@@ -13,11 +13,15 @@ public class EmployeeAdd {
                 .buildSessionFactory();
         try {
             Session session = factory.getCurrentSession();
-            Employee emp = new Employee("Irina", "Volobueva", "HR", 450);
             session.beginTransaction();
-            session.save(emp);
+
+//            Employee employee = session.get(Employee.class,4);
+//            session.delete(employee);
+
+            session.createQuery("delete Employee " + "where department = 'HR'").executeUpdate();
+
             session.getTransaction().commit();
-            System.out.println("Done");
+
         }
         finally {
             factory.close();
