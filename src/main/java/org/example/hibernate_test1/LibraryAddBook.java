7ca8b226c4d8246c8a44a8bf851cd7fd2b365493
Egor <sartvas@yandex.ru>
2021-08-20 17:28:22 +0300

    Homework: Create library

---
+++ src/main/java/org/example/hibernate_test1/LibraryAddBook.java
@@ -0,0 +1,26 @@
+package org.example.hibernate_test1;
+
+import org.hibernate.Session;
+import org.example.hibernate_test1.entity.Book;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class LibraryAddBook {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration().configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Book.class)
+                .buildSessionFactory();
+        try {
+            Session session = factory.getCurrentSession();
+            Book book = new Book("Lukomorje", "Pushkin A.S.", "Poem", 750);
+            session.beginTransaction();
+            session.save(book);
+            session.getTransaction().commit();
+            System.out.println("The book name " + "'" + book.getName() + "'" +" was added in the library!");
+
+        } finally {
+            factory.close();
+        }
+
+    }
+}
